942f47e759550028f8d82e81b4a4cebaaebb5bf3
kaitlinqv <kaitlin.qvale@gmail.com>
2016-02-09 12:18:40 -0800

    gitTest
    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+//class code by Kaitlin Q
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!"); // easy hello world to test github
+        //for some reason even when searching my computer i cannot find the git.exe to link it to intellij.. so that kinda blows.
+    }
+}
