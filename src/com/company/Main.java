9774fe11c81328106cd4b881f5dca21a11da32f8
kaitlinqv <kaitlin.qvale@gmail.com>
2016-02-11 10:15:04 -0800

    somethig

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello World!"); // easy hello world to test github
         //for some reason even when searching my computer i cannot find the git.exe to link it to intellij.. so that kinda blows.
+        SOMETHIN DIFFERENT INCORRECT CODE
+                
     }
 }
